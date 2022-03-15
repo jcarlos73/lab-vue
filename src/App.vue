c28f7971fe72095b41decd14984c369b00f54e63
Juan Carlos <jcparetas@gmail..com>
2022-03-15 09:35:50 +0100

    done

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h1>{{ text }}</h1>
+    <Navbar />
+    <p :class="myClass">Este párrafo cambia de color</p>
+    <button @click="button">Haz click aqui</button>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar';
+export default {
+  name: 'App',
+  data () {
+    return{
+     text: 'Bienvenido a mi página',
+     myClass: 'red'
+    }
+  },
+  components: {
+    Navbar
+  },
+
+  methods:{
+    button  (){
+      this.myClass = 'green';
+    }
+  }
+}
+</script>
+
+<style>
+  .red {
+    color: red;
+  }
+
+  .green {
+    color: green;
+  }
+</style>
